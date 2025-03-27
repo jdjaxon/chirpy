0dfdcae6c1dfaf2602d05e4578fecdd7a52d3581
Jeremy Jackson <87815518+jdjaxon@users.noreply.github.com>
2025-03-27 11:04:21 -0400

    Adding server boilerplate as starting point for the HTTP server.

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"net/http"
+)
+
+func main() {
+	serverMux := http.NewServeMux()
+	server := http.Server{
+		Addr:    ":8080",
+		Handler: serverMux,
+	}
+
+	server.ListenAndServe()
+}
