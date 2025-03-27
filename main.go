70e698973f7ec278ad0737fb0c0ebb4b6fba3f4a
Jeremy Jackson <87815518+jdjaxon@users.noreply.github.com>
2025-03-27 14:44:45 -0400

    Added handler for healthcheck.

--- main.go
+++ main.go
@@ -1,16 +1,28 @@
 package main
 
 import (
+	"log"
 	"net/http"
 )
 
 func main() {
+	serverRootDir := "."
+	port := "8080"
 	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir(".")))
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(serverRootDir))))
+	mux.HandleFunc("/healthz", handlerHealthcheck)
+
 	server := &http.Server{
-		Addr:    ":8080",
+		Addr:    ":" + port,
 		Handler: mux,
 	}
 
+	log.Printf("Serving files from %s on port %s\n", serverRootDir, port)
 	server.ListenAndServe()
+}
+
+func handlerHealthcheck(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("content-type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(http.StatusOKText))
 }
