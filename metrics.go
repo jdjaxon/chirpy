da94d6bfacfa81250316b19dc3aceb57cbf5ff10
Jeremy Jackson <87815518+jdjaxon@users.noreply.github.com>
2025-03-30 11:40:44 -0400

    Refactored metrics and associated middleware logic into its own file.

---
+++ metrics.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"net/http"
+)
+
+func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("content-type", "text/html; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	header := fmt.Sprintf("<h1>Welcome, Chirpy Admin</h1>")
+	metrics := fmt.Sprintf("<p>Chirpy has been visited %d times!</p>", cfg.fileserverHits.Load())
+	resp := fmt.Sprintf("<html><body>%s%s</html></body>", header, metrics)
+	w.Write([]byte(resp))
+}
+
+func (cfg *apiConfig) handlerResetMetrics(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits.Store(0)
+	w.Header().Set("content-type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Reset hits to 0"))
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
