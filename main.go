c062d6b1ad7ecf7b164bb654ca48a493386f923f
Jeremy Jackson <87815518+jdjaxon@users.noreply.github.com>
2025-03-29 22:24:15 -0400

    Added a middleware layer to track /app/ endpoint metrics. Also added /metrics and /reset endoints to show and reset metrics, respectively.

--- main.go
+++ main.go
@@ -1,16 +1,33 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
+
+const appEndpoint = "/app"
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
 
 func main() {
 	serverRootDir := "."
 	port := "8080"
 	mux := http.NewServeMux()
-	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(serverRootDir))))
+	cfg := &apiConfig{}
+
+	mux.Handle(
+		appEndpoint+"/",
+		cfg.middlewareMetricsInc(
+			http.StripPrefix(appEndpoint, http.FileServer(http.Dir(serverRootDir))),
+		),
+	)
 	mux.HandleFunc("/healthz", handlerHealthcheck)
+	mux.HandleFunc("/metrics", cfg.handlerGetMetrics)
+	mux.HandleFunc("/reset", cfg.handlerResetMetrics)
 
 	server := &http.Server{
 		Addr:    ":" + port,
@@ -24,5 +41,24 @@ func main() {
 func handlerHealthcheck(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("content-type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(http.StatusOKText))
+	w.Write([]byte(http.StatusText(http.StatusOK)))
+}
+
+func (cfg *apiConfig) handlerGetMetrics(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("content-type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(fmt.Sprintf("Hits: %v", cfg.fileserverHits.Load())))
+}
+
+func (cfg *apiConfig) handlerResetMetrics(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits.Store(0)
+	w.Header().Set("content-type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
 }
