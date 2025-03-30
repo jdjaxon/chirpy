eb41df24598821000495677a26a90f3390903ae6
Jeremy Jackson <87815518+jdjaxon@users.noreply.github.com>
2025-03-30 11:40:05 -0400

    Refactored healthcheck handler into its own file.

---
+++ health.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"net/http"
+)
+
+func handlerHealthcheck(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("content-type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(http.StatusText(http.StatusOK)))
+}
