f6e357621c0a200a79d4fae41146948996f9a6d5
Jeremy Jackson <87815518+jdjaxon@users.noreply.github.com>
2025-03-30 11:44:56 -0400

    Extracted logic for metric and health endpoints into separate files. Added logging around ListenAndServe call.

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 	"sync/atomic"
@@ -25,9 +24,9 @@ func main() {
 			http.StripPrefix(appEndpoint, http.FileServer(http.Dir(serverRootDir))),
 		),
 	)
-	mux.HandleFunc("/healthz", handlerHealthcheck)
-	mux.HandleFunc("/metrics", cfg.handlerGetMetrics)
-	mux.HandleFunc("/reset", cfg.handlerResetMetrics)
+	mux.HandleFunc("GET /api/healthz", handlerHealthcheck)
+	mux.HandleFunc("GET /admin/metrics", cfg.handlerMetrics)
+	mux.HandleFunc("POST /admin/reset", cfg.handlerResetMetrics)
 
 	server := &http.Server{
 		Addr:    ":" + port,
@@ -35,30 +34,5 @@ func main() {
 	}
 
 	log.Printf("Serving files from %s on port %s\n", serverRootDir, port)
-	server.ListenAndServe()
-}
-
-func handlerHealthcheck(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("content-type", "text/plain; charset=utf-8")
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(http.StatusText(http.StatusOK)))
-}
-
-func (cfg *apiConfig) handlerGetMetrics(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("content-type", "text/plain; charset=utf-8")
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(fmt.Sprintf("Hits: %v", cfg.fileserverHits.Load())))
-}
-
-func (cfg *apiConfig) handlerResetMetrics(w http.ResponseWriter, r *http.Request) {
-	cfg.fileserverHits.Store(0)
-	w.Header().Set("content-type", "text/plain; charset=utf-8")
-	w.WriteHeader(http.StatusOK)
-}
-
-func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		cfg.fileserverHits.Add(1)
-		next.ServeHTTP(w, r)
-	})
+	log.Fatal(server.ListenAndServe())
 }
