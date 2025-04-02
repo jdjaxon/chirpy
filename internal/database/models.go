84d7d3593c23a5ac866d9829460360e66e19a4d3
Jeremy Jackson <87815518+jdjaxon@users.noreply.github.com>
2025-04-02 15:13:46 -0400

    Create insert query for new chirps and generated the corresponding Go logic.

--- internal/database/models.go
+++ internal/database/models.go
@@ -5,10 +5,19 @@
 package database
 
 import (
+	"database/sql"
 	"time"
 
 	"github.com/google/uuid"
 )
+
+type Chirp struct {
+	ID        uuid.UUID
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	Body      sql.NullString
+	UserID    uuid.NullUUID
+}
 
 type User struct {
 	ID        uuid.UUID
--- sql/schema/002_chirps.sql
+++ sql/schema/002_chirps.sql
@@ -3,7 +3,7 @@ CREATE TABLE chirps(
     id UUID PRIMARY KEY,
     created_at TIMESTAMP NOT NULL,
     updated_at TIMESTAMP NOT NULL,
-    body TEXT NOT NULL UNIQUE,
+    body TEXT,
     user_id UUID REFERENCES users(id) ON DELETE CASCADE
 );
 
