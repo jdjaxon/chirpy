2552c6d0e6a2faae1f52e2baa205ed7385b7b8c2
Jeremy Jackson <87815518+jdjaxon@users.noreply.github.com>
2025-04-04 12:02:41 -0400

    Added internal auth package to hash and check hashed passwords.

---
+++ internal/auth/auth.go
@@ -0,0 +1,25 @@
+package auth
+
+import (
+	"fmt"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+func HashPassword(password string) (string, error) {
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return "", fmt.Errorf("could not hash password: %w", err)
+	}
+
+	return string(hashedPassword), nil
+}
+
+func CheckPasswordHash(hash, password string) error {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
