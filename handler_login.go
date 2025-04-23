da42da24442d75cd1bd1ab1f3a6cfa583f19cb44
Jeremy Jackson <87815518+jdjaxon@users.noreply.github.com>
2025-04-23 11:11:46 -0400

    Implemented JWT logic for handling user logins.

--- handler_login.go
+++ handler_login.go
@@ -3,14 +3,16 @@ package main
 import (
 	"encoding/json"
 	"net/http"
+	"time"
 
 	"github.com/jdjaxon/chirpy/internal/auth"
 )
 
 func (cfg *apiConfig) handlerLogin(w http.ResponseWriter, r *http.Request) {
 	type request struct {
-		Email    string `json:"email"`
-		Password string `json:"password"`
+		Email            string `json:"email"`
+		Password         string `json:"password"`
+		ExpiresInSeconds *int   `json:"expires_in_seconds,omitempty"`
 	}
 
 	decoder := json.NewDecoder(r.Body)
@@ -48,11 +50,32 @@ func (cfg *apiConfig) handlerLogin(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	expiresIn := time.Second * 3600
+	if req.ExpiresInSeconds != nil {
+		reqExpiry := time.Duration(*req.ExpiresInSeconds) * time.Second
+		if reqExpiry <= time.Second*3600 {
+			expiresIn = reqExpiry
+		}
+	}
+
+	// MakeJWT(userID uuid.UUID, tokenSecret string, expiresIn time.Duration) (string, error)
+	userToken, err := auth.MakeJWT(user.ID, cfg.tokenSecret, expiresIn)
+	if err != nil {
+		respondWithError(
+			w,
+			http.StatusUnauthorized,
+			err,
+			"Failed to create token",
+		)
+		return
+	}
+
 	resp := User{
 		ID:        user.ID,
 		CreatedAt: user.CreatedAt,
 		UpdatedAt: user.UpdatedAt,
 		Email:     user.Email,
+		Token:     userToken,
 	}
 
 	respondWithJSON(w, http.StatusOK, resp)
