2b3ecca5f989b5f20ae0ef2b986e4123123a65e5
Jeremy Jackson <87815518+jdjaxon@users.noreply.github.com>
2025-05-01 15:14:57 -0400

    Add queries for refresh tokens.

---
+++ sql/queries/refresh_tokens.sql
@@ -0,0 +1,19 @@
+-- name: CreateRefreshToken :exec
+INSERT INTO refresh_tokens (token, created_at, updated_at, user_id, expires_at)
+VALUES (
+    $1,
+    now(),
+    now(),
+    $2,
+    $3
+);
+
+-- name: GetRefreshToken :one
+SELECT *
+FROM refresh_tokens
+WHERE token = $1;
+
+-- name: RevokeRefreshToken :exec
+UPDATE refresh_tokens
+SET revoked_at = now(), updated_at = now()
+WHERE token = $1;
